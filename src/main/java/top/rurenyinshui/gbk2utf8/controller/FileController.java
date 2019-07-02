ac8d247d02b936a82b3f386f1575049a3077c6ea
lixin <101311981@qq.com>
2019-07-02 22:58:54 +0800

    上传下载

--- src/main/java/top/rurenyinshui/gbk2utf8/controller/FileController.java
+++ src/main/java/top/rurenyinshui/gbk2utf8/controller/FileController.java
@@ -1,14 +1,16 @@
 package top.rurenyinshui.gbk2utf8.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import top.rurenyinshui.gbk2utf8.DTO.Response;
 
-import java.io.File;
-import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 
 /**
  * 包名: top.rurenyinshui.gbk2utf8.controller
@@ -50,6 +52,50 @@ public class FileController {
         return new Response("上传失败");
     }
 
-
+    @GetMapping("/download")
+    public String downloadFile(HttpServletRequest request, HttpServletResponse response) {
+        String fileName = "a.txt";// 文件名
+        if (fileName != null) {
+            //设置文件路径
+            String realPath = "D:\\ideaworkspace\\sth\\gbk2utf8\\src\\main\\resources\\static\\";
+            File file = new File(realPath , fileName);
+            if (file.exists()) {
+                response.setContentType("application/force-download");// 设置强制下载不打开
+                response.addHeader("Content-Disposition", "attachment;fileName=" + fileName);// 设置文件名
+                byte[] buffer = new byte[1024];
+                FileInputStream fis = null;
+                BufferedInputStream bis = null;
+                try {
+                    fis = new FileInputStream(file);
+                    bis = new BufferedInputStream(fis);
+                    OutputStream os = response.getOutputStream();
+                    int i = bis.read(buffer);
+                    while (i != -1) {
+                        os.write(buffer, 0, i);
+                        i = bis.read(buffer);
+                    }
+                    return "下载成功";
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (bis != null) {
+                        try {
+                            bis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (fis != null) {
+                        try {
+                            fis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+        return "下载失败";
+    }
 
 }
